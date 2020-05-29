a9a7348ee060ec0b85ca564f6b66f4016fe7f3cd
recursivecurse <adityakumarm.cs18@rvce.edu.in>
2020-05-29 18:47:39 +0530

    Java Lab files

---
+++ package/Main.java
@@ -0,0 +1,12 @@
+import Rvce.Welcome;
+import java.io.*;
+
+public class Main
+{
+	public static void main(String args[])
+	{
+		Welcome obj = new Welcome();
+		obj.welcomeMsg();
+	}
+}
+
---
+++ package/Rvce/Welcome.txt
@@ -0,0 +1,12 @@
+package Rvce;
+
+import java.io.*;
+
+public class Welcome
+{
+	public void welcomeMsg()
+	{
+		System.out.println("Welcome to RVCE");
+	}
+}
+
